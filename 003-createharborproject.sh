ce96e2c916f85ad22b5fe7f518c97e30e75ac629
Brett Tackaberry <tackaberry@google.com>
2025-11-14 01:53:46 +0000

    eource .env

--- 003-createharborproject.sh
+++ 003-createharborproject.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
 
+source .env
+
 gdcloud harbor harbor-projects create ${HARBOR_PROJECT} \
 --instance ${HARBOR_INSTANCE} \
 --project ${HARBOR_INSTANCE_PROJECT}
--- 004-addharborsecret.sh
+++ 004-addharborsecret.sh
@@ -1,3 +1,5 @@
 #!/bin/bash
 
+source .env
+
 ku create secret docker-registry ${HARBOR_SECRET} --from-file=.dockerconfigjson=$HOME/.docker/config.json
